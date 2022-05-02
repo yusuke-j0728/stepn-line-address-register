60f4506ec0eea5cc1ac689e455bedeccf484d42a
yusuke-j0728 <yusuke.j0728@gmail.com>
2022-05-03 00:45:19 +0900

    20220503 index.js edit

--- docs/index.js
+++ docs/index.js
@@ -4,10 +4,10 @@ var params = (new URL(document.location)).searchParams;
 $(function () {
     // 送信
     $('form').submit(function () {
-        var inquiry = $('textarea[name="inquiry"]').val();
-        var email = $('input[name="email"]').val();
+        var address = $('input[name="address"]').val();
+        // var email = $('input[name="email"]').val();
         // var msg = `${key}\nお問い合わせ内容：${inquiry}`;
-        var msg = `アドレス登録：\n${inquiry}`;
+        var msg = `アドレス登録：\n${address}`;
         sendText(msg);
 
         return false;
