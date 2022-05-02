2f10f02a50fc286c3d960fc3445e47d8fa2cfb41
yusuke-j0728 <yusuke.j0728@gmail.com>
2022-05-03 00:43:18 +0900

    20220503-05 github page msg update

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,13 @@
 var params = (new URL(document.location)).searchParams;
-var key = params.get('key');
+// var key = params.get('key');
 
 $(function () {
     // 送信
     $('form').submit(function () {
         var inquiry = $('textarea[name="inquiry"]').val();
         var email = $('input[name="email"]').val();
-        var msg = `${key}\nお問い合わせ内容：${inquiry}`;
+        // var msg = `${key}\nお問い合わせ内容：${inquiry}`;
+        var msg = `アドレス登録：\n${inquiry}`;
         sendText(msg);
 
         return false;
